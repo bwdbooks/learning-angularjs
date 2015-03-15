b1bed0d06a95d511d200a50e9adc79a2a24fca69
Ronny De Winter <ronny.de.winter@gmail.com>
2015-03-15 11:32:51 +0100

    remove  parameter in ng-click function call (as in the book)

--- ch04/js/scope_template.js
+++ ch04/js/scope_template.js
@@ -3,7 +3,7 @@ angular.module('myApp', []).
     $scope.valueA = 5;
     $scope.valueB = 7;
     $scope.valueC = 12;
-    $scope.addValues = function(v1, v2, e) {
+    $scope.addValues = function(v1, v2) {
       var v = angular.$rootScope;
       $scope.valueC = v1 + v2;
     };
